f05c9d7881e975952eebc95359329268c0abc9aa
Matt Kaniaris <mkaniaris@gmail.com>
2012-05-11 15:35:08 -0700

    fully functioning burndown graphs

--- static/js/burndown.js
+++ static/js/burndown.js
@@ -9,42 +9,45 @@ google.setOnLoadCallback(drawChart);
 // draws it.
 function drawChart() {
 
-  var chart = [];
-  var options;
-  var last_value = [];
-  var i;
-  var max = 0;
-  var _chart;
-
   //find max length
-
-  $.each(burndown_data, function(sprint, values){
-    if (values.length > max){
-      max = values.length;
-    }
-  });
-  //push arrays onto charts
-  i = max -1;
-  while (i>=0){
-    chart[i] = [i];
-    i -=1;
-  }
-  // push data
-  chart[0] = ['Day'];
-  $.each(burndown_data, function(sprint, values){
-    var value;
-    chart[0].push(sprint);
-    for (i =1; i < max; i++){
-      if (values[i]){
-        value = values[i];
+  var make_chart = function(title, data, location){
+    var chart = [];
+    var options;
+    var last_value = [];
+    var i;
+    var max = 0;
+    var _chart;
+    $.each(data, function(sprint, values){
+      if (values.length > max){
+        max = values.length;
       }
-      chart[i].push(value);
+    });
+    //push arrays onto charts
+    i = max -1;
+    while (i>=0){
+      chart[i] = [i];
+      i -=1;
     }
-  });
-  options = {'title': "Defects Burndown",'width':400,'height':300};
-  // Instantiate and draw our chart, passing in some options.
-  _chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-  _chart.draw(google.visualization.arrayToDataTable(chart), options);
-
+    // push data
+    chart[0] = ['Day'];
+    $.each(data, function(sprint, values){
+      var value;
+      chart[0].push(sprint);
+      for (i =1; i < max; i++){
+        if (values[i]){
+          value = values[i];
+        }
+        chart[i].push(value);
+      }
+    });
+    options = {'title': title + " Burndown",'width': 550,'height':350};
+    // Instantiate and draw our chart, passing in some options.
+    _chart = new google.visualization.LineChart($(location)[0]);
+    _chart.draw(google.visualization.arrayToDataTable(chart), options);
+  };
+  if (burndown_data){
+    make_chart("Defects", burndown_data.defects, "#defects-chart");
+    make_chart("Tasks", burndown_data.tasks, "#tasks-chart");
+  }
 }
 
