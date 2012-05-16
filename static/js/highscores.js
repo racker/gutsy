36e4d795cd2bbc5b9fdd19214d9b4bf4b2072583
Matt Kaniaris <mkaniaris@gmail.com>
2012-05-16 13:47:23 -0700

    broken temp commit

--- static/js/highscores.js
+++ static/js/highscores.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
   $("#lordly-title").click(function(){
-    $("div#title-maker").dialog({modal: true, height: 280, width: 430});
-  });
-
-  $("div#title-maker").submit(function(event){
-    // do stuff
-    return false;
+    $("div#title-maker").dialog({modal: true,  width: 470});
   });
 });
