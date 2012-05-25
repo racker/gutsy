8c961880204bf8ae8e6d16965ca2c362bc1b388c
Geoff Greer <geoff@greer.fm>
2012-05-24 18:24:09 -0700

    Better global expanding/collapsing

--- static/js/defects.js
+++ static/js/defects.js
@@ -5,22 +5,33 @@
  * Clicking on the numbers displays all event information.
  */
 $(document).ready(function() {
+  var collapsed = "\u25B6";
+  var expanded = "\u25BC";
   $(".bucket").css("cursor", "pointer").click(function () {
     $(this).next().children().each(function() {
       $(this).toggle();
     });
     $(this).children(".arrow").each(function() {
-      if ($(this).text() === "\u25B6") {
-        $(this).text("\u25BC");
+      if ($(this).text() === collapsed) {
+        $(this).text(expanded);
       } else {
-        $(this).text("\u25B6");
+        $(this).text(collapsed);
       }
     });
   });
   $(".title").css("cursor", "pointer").click(function () {
-    $(this).next().children().children().each(function() {
-      $(this).show();
+    $(this).children(".arrow").each(function() {
+      var arrows = $(this).parent().next().children().children(".arrow");
+      var tables = $(this).parent().next().children(".table").children();
+      if ($(this).text() === collapsed) {
+        tables.show();
+        arrows.text(expanded);
+        $(this).text(expanded);
+      } else {
+        tables.hide();
+        arrows.text(collapsed);
+        $(this).text(collapsed);
+      }
     });
   });
 });
-
