a7797c5928a8719f4f3e29c571ab47737150da22
Geoff Greer <geoff@greer.fm>
2012-06-01 16:25:19 -0700

    Store crawl errors in db. Display them on dashboard

--- static/js/meta_index.js
+++ static/js/meta_index.js
@@ -3,3 +3,10 @@ $(document).ready(function(){
     $("div#devops_url_editor-" + $(this).val()).dialog({modal: true, width: '256px'});
   });
 });
+
+$(document).ready(function(){
+  $(".dialog").click(function(){
+    var node = $(this).children("div:hidden");
+    $(node).val().dialog({modal: true, width: '256px'});
+  });
+});
