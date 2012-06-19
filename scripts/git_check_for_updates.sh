111f156c79813f66648854cd2c6275f3dd79a10c
Geoff Greer <geoff@greer.fm>
2012-06-19 14:38:34 -0700

    Tweak this script so it works.

--- scripts/git_check_for_updates.sh
+++ scripts/git_check_for_updates.sh
@@ -1,19 +1,20 @@
 #/bin/bash
-function changes_in_git_repo {
-  latestlocal=`git rev-parse HEAD`;
-  echo $latestlocal
-  gitrepourl=`git remote -v | grep fetch | awk '{print $2}'`;
-  echo $gitrepourl;
 
-  latestremote=`git ls-remote --heads $gitrepourl master| awk '{print $1}'`;
-  echo $latestremote;
+cd /data/gutsy
 
-  if [ $latestlocal != $latestremote ]
-  then
+latestlocal=`git rev-parse HEAD`;
+echo $latestlocal
+gitrepourl=`git remote -v | grep fetch | awk '{print $2}'`;
+echo $gitrepourl;
+
+latestremote=`git ls-remote --heads $gitrepourl master| awk '{print $1}'`;
+echo $latestremote;
+
+if [ $latestlocal != $latestremote ]
+then
     echo "Changes since last build!";
-    `git pull && /etc/init.d/gutsy restart`
-  else
+    git pull && /etc/init.d/gutsy restart
+    chown -R gutsy: .
+else
     echo "No changes since last build";
-  fi
-}
-changes_in_git_repo;
+fi
