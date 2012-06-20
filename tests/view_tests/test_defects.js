11e0239f3ad18c5d244632ada298886be67c6a2f
Geoff Greer <geoff@greer.fm>
2012-06-20 09:12:50 -0700

    Nuke old tests.

--- tests/view_tests/test_defects.js
+++
@@ -1,6 +0,0 @@
-var base = require('./base');
-var middleware = require('web/middleware');
-
-exports.test_example_full_only_version_one = function(test, assert) {
-  base.test_view(test, assert, 'defects.jade', 'example-full.json');
-};
--- tests/view_tests/test_devhealth.js
+++
@@ -1,6 +0,0 @@
-var base = require('./base');
-var middleware = require('web/middleware');
-
-exports.test_example_full_only_github = function(test, assert) {
-  base.test_view(test, assert, 'devhealth.jade', 'example-full.json');
-};
