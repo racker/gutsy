9dc80400d6bffba29a6762ccc8e18dcbbad4bda2
Felix Sargent <felix.sargent@gmail.com>
2012-06-27 17:17:18 -0700

    Defects/Add Defects \n\n Added form to create defects. UI is correct, though would like to align right -properly.

--- static/js/defects.js
+++ static/js/defects.js
@@ -16,13 +16,15 @@ function toggle_arrows() {
 }
 
 $(document).ready(function() {
-  $("#defects").children(".bucket").css("cursor", "pointer").click(function () {
+  // Buckets dropdown control
+  $("#defects_chart").children(".bucket").css("cursor", "pointer").click(function () {
     $(this).next().children().each(function() {
       $(this).toggle();
     });
     $(this).children(".arrow").each(toggle_arrows);
   });
-  $(".title").css("cursor", "pointer").click(function () {
+  // All Buckets dorpdown control
+  $("#defects_title").css("cursor", "pointer").click(function () {
     $(this).children(".arrow").each(function() {
       var arrows = $(this).parent().next().children().children(".arrow");
       var tables = $(this).parent().next().children(".table").children();
@@ -37,4 +39,37 @@ $(document).ready(function() {
       }
     });
   });
+
+function create_defect_scope_dropdown() {
+  if (burndown_data){
+      $.each(burndown_data, function (scope, data) {
+        $.each(data, function (asset_type, assets) {
+          var div_id;
+          var scope_name = scope.replace(/\W/g, "-").toLowerCase();
+          select_options[scope_name] = scope;
+          div_id = asset_type + "-chart-" + scope_name;
+          $("#charts").append("<div id='" + div_id +"' class='burndown-chart " + scope_name + "-chart'></div>");
+          make_chart(asset_type, scope, assets, div_id);
+        });
+      });
+      for (var key in select_options) {
+        var option = $("<option value='" + key + "'>" + select_options[key] + "</option>");
+        if (key === "total") {
+          option.attr("selected", "selected");
+        }
+        $("#scope-picker").append(option);
+      }
+      $("#scope-picker").append("<option value='burndown'>Show all</option>");
+      $("#scope-picker").change(function (event) {
+        $(".burndown-chart").hide();
+        $("." + $(this).val() + "-chart").show();
+      });
+    }
+
+    $("#charts").children(".bucket").css("cursor", "pointer").click(function () {
+      $(this).next().toggle();
+      $(this).children(".arrow").each(toggle_arrows);
+    });
+  }
+
 });
