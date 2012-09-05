f9059218392a5e4a175fa26d7170022b337b5c0e
Matt Kaniaris <mkaniaris@gmail.com>
2012-09-05 12:55:14 -0700

    now with an arc!

--- static/js/arcify.js
+++ static/js/arcify.js
@@ -2,98 +2,86 @@ $(document).ready(function(){
   //var nodes = [{"name": "a", "color": "#ff2299"}, {"name": "b", "color": "#9922ff"}, {"name": "c", "color": "#ff1122"}];
   //var edges = [{"source": 0, "target": 1, "value": 8}];
 
-  var _nodes = [];
-  $("#hidden .node").each(function(e) {
-    _nodes.push({name: $(this).children(".name").text(), color: $(this).children(".color").text()});
-  });
+var w = 900, h = vertexes.length * 220;
 
-  var _edges = [];
-  $("#hidden .edge").each(function(e) {
-    var children = $(this).children();
-    var source = children.filter(".source").text();
-    var target = children.filter(".target").text();
-    if (source !== "" && target !== "") {
-      _edges.push({source: parseInt(source, 10), target: parseInt(target, 10), value: 8});
-    }
-  });
-
-var w = 900, h = 300;
-
-var force = d3.layout.force()
-    .nodes(d3.values(_nodes))
-    .links(_edges)
-    .size([w, h])
-    .linkDistance(150)
-    .charge(-600)
-    .on("tick", tick)
-    .start();
+// var force = d3.layout.force()
+//   .nodes(d3.values([]))
+//   .links([])
+//   .size([w, h])
+//   .linkDistance(150)
+//   .charge(-600)
+//   .on("tick", tick)
+//   .start();
 
 var svg = d3.select("#chart").append("svg:svg")
-    .attr("width", "100%")
-    .attr("height", h)
-    .style("padding", "20px");
+  .attr("width", "100%")
+  .attr("height", h)
+  .style("padding", "20px");
 
-// Per-type markers, as they don't inherit styles.
-svg.append("svg:defs").selectAll("marker")
-    .data(["arrow"])
-  .enter().append("svg:marker")
-    .attr("id", String)
-    .attr("viewBox", "0 -5 10 10")
-    .attr("refX", 15)
-    .attr("refY", -1.5)
-    .attr("markerWidth", 6)
-    .attr("markerHeight", 10)
-    .attr("orient", "auto")
-    .style("fill", "#FAA")
-  .append("svg:path")
-    .attr("d", "M0,-5L10,0L0,5");
+var y = d3.scale.ordinal()
+  .domain(vertexes)
+  .rangeBands([0, h]);
 
-var path = svg.append("svg:g").selectAll("path")
-    .data(force.links())
-  .enter().append("svg:path")
-    .attr("class", function(d) { return "link arrow"; })
-    .attr("marker-mid", function(d) { return "url(#arrow)"; });
+var x = 0;
 
- var node = svg.selectAll("g.node")
-      .data(_nodes)
-    .enter().append("svg:g")
-      .attr("class", "node")
-      .call(force.drag);
+var vertex_group = svg.append("g");
+var vertex_h = 220;
+var vertex_w = 360;
 
-  var text_width = function(txt) {
-    var e = $("<span>"+txt+"</span>").css("font-size", "1.5em");
-    $("body").append(e)
-    var width = e.width();
-    e.remove();
-    return width;
-  };
+//plot the team logos along the x-axis
+vertex_group.selectAll("text")
+  .data(vertexes)
+  .enter()
+  .append("text")
+  .text(String)
+  .attr("dy", ".5em")
+  .style("fill", '#333')
+  .attr("y", y)
+  .attr("x", x)
+  .attr("width", vertex_w)
+  .attr("height", vertex_h);
+  //.attr("xlink:href",function(d, i) { return edges[i];} );
+  //fades out all other trades, except for ones involving this team
+  // .on("mouseover", fade(0.1,teams))
+  // //brings all the faded out trades back to normal opacity
+  // .on("mouseout", fade(1,teams));
 
-  node.append("svg:rect")
-      .attr("width", function(d) { return text_width(d.name) + 4; })
-      .attr("height", "2em")
-      .attr("x", function(d) {return -0.5 * (text_width(d.name) + 2); })
-      .attr("y", "-1.4em")
-      .style("fill", function(d) { return d.color; });
+var arcGroup = svg.append("g");
+//draw the arcs from one team to the other
+//to represent a trade
+arcGroup.selectAll("path")
+  .data(edges)
+  .enter().append("path")
+  .attr("d",function(d, i){
+    var rx, ry, x = vertex_w+30;
+    //swap the values if neccessary
+    var proj_a_y = y(d[0]);
+    var proj_b_y = y(d[1]);
+    if(proj_a_y > proj_b_y){
+    y1 = proj_a_y;
+    y2 = proj_b_y;
+    }
+    else{
+    y1 = proj_b_y;
+    y2 = proj_a_y;
+    }
+    //start the arc at the middle of the logo
+    y1 = y1;
+    y2 = y2;
+    //qick calculation for the arc. The closer the
+    //teams are to each other (on the axis), the
+    //smaller the radii need to be
+    val = (y2 - y1)/2;
+    rx = val;
+    ry = val;
 
-  node.append("svg:text")
-      .text(function(d) { return d.name; })
-      .style("font-size", "1.5em")
-      .style("text-anchor", "middle");
-
-  // Use elliptical arc path segments to doubly-encode directionality.
-  function tick() {
-    path.attr("d", function(d) {
-      var dx = d.target.x - d.source.x,
-          dy = d.target.y - d.source.y,
-          dr = Math.sqrt(dx * dx + dy * dy),
-          hx = d.source.x + dx/2.0;
-          hy = d.source.y + dy/2.0;
-      return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + hx + "," + hy + "A" + dr + "," + dr + " 0 0,0 " + d.target.x + "," + d.target.y;
-    });
-
-    node.attr("transform", function(d) {
-      return "translate(" + d.x + "," + d.y + ")";
-    });
-  }
+    return "M" + x + "," + y1 + " A "+ rx + "," + ry +" 0 0 0 " + x + "," + y2;
+  })
+  .attr("stroke", function(d,i){ return "#000";})
+  //set the line thickness based on the 'size' of the trade.
+  //the more players/picks exchanged, the thicker the line
+  .attr("stroke-width","15px")
+  .attr("fill","none");
+  //on click, update the view model
 });
 
